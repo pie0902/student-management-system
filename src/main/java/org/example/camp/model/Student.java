774c061aa002af55589537618328c5036be44a66
pie0902 <zhfoqkq@gmail.com>
2024-01-11 10:16:59 +0900

    팀과제 시작

---
+++ src/main/java/org/example/camp/model/Student.java
@@ -0,0 +1,21 @@
+package org.example.camp.model;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+
+    public Student(String seq, String studentName) {
+        this.studentId = seq;
+        this.studentName = studentName;
+    }
+
+    // Getter
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+}
